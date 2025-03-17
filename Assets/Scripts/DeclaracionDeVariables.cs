3dd8765e3f7cc69001208553c3003aca4d1e7519
Ramiro08ds <Sanchezriverazaied@gmail.com>
2025-03-17 09:07:21 -0300

    Variables declaradas y subidas a la consola

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DeclaracionDeVariables : MonoBehaviour
 {
+    int EscenasMalas = 8;
+    string NombrePelicula = "Minecraft";
+    float AlturaSteve = 1.90f;
+    bool EsBuena = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(EscenasMalas);
+        Debug.Log(NombrePelicula);
+        Debug.Log(AlturaSteve);
+        Debug.Log(EsBuena);
     }
 
     // Update is called once per frame
