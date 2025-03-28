786ae1a2d722cfc58490ee5b3f734d2b72fa97bf
Ramiro08ds <Sanchezriverazaied@gmail.com>
2025-03-28 10:44:08 -0300

    Ej 4 guia

---
+++ Assets/Scripts/CalculosPersonalizados.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculosPersonalizados : MonoBehaviour
+
+{
+    public float num1;
+    public float num2;
+     float resultadoSuma;
+     float resultadoResta;
+     float resultadoMultiplicacion;
+     float resultadoDivision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultadoDivision = num1 / num2;
+        resultadoMultiplicacion = num1 * num2;
+        resultadoResta = num1 - num2;
+        resultadoSuma = num1 + num2;
+        Debug.Log("el resultado de la suma entre" + num1 + " y " + num2 + " es " + resultadoSuma);
+        Debug.Log("el resultado de la resta entre" + num1 + " y " + num2 + " es " + resultadoResta);
+        Debug.Log("el resultado de la multiplicacion entre" + num1 + " y " + num2 + " es " + resultadoMultiplicacion);
+        Debug.Log("el resultado de la division entre" + num1 + " y " + num2 + " es " + resultadoDivision);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
