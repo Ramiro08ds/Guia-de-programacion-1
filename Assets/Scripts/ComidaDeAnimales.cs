23f9b3a029b64c07dab6c640cb15e6e4432c438b
Ramiro08ds <Sanchezriverazaied@gmail.com>
2025-04-04 10:41:33 -0300

    Simulacro de prueba 1

---
+++ Assets/Scripts/ComidaDeAnimales.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaDeAnimales : MonoBehaviour
+{
+    public string CodigoAnimal;
+    public int CantidadDias;
+    int cantidadComidaTotal;
+    int racionGatos = 300;
+    int racionPerrosChicos = 400;
+    int racionPerrosGrandes = 700;
+    int racionAnimalActual;
+    float costoAlimento;
+    float precioPorcienG = 80;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CodigoAnimal == "G" || CodigoAnimal =="g")
+        {
+            racionAnimalActual = racionGatos;
+        }
+        else if (CodigoAnimal == "PP" || CodigoAnimal == "pp")
+        {
+            racionAnimalActual = racionPerrosChicos;
+        }
+        else if (CodigoAnimal == "PG" ||CodigoAnimal == "pg")
+        {
+            racionAnimalActual = racionPerrosGrandes;
+
+        }
+        else {
+            Debug.Log("tipo de animal mal ingresado, por favor intente otra vez, igrese PG para perro grande, PP para perro pequeño y G para un gato");
+        }
+        cantidadComidaTotal = racionAnimalActual * CantidadDias;
+        
+        Debug.Log("para ese periodo se necesitan: " + cantidadComidaTotal + " gramos de alimento");
+        costoAlimento = cantidadComidaTotal / 100 * precioPorcienG;
+        Debug.Log(" el precio es de: " + costoAlimento + " pesos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
