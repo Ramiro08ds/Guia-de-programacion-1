4ba3853c5d6f2751284c7f44414bcfe446c9f150
Ramiro08ds <Sanchezriverazaied@gmail.com>
2025-04-07 08:39:49 -0300

    Terminado comida de animalitos

--- Assets/Scripts/ComidaDeAnimales.cs
+++ Assets/Scripts/ComidaDeAnimales.cs
@@ -16,27 +16,35 @@ public class ComidaDeAnimales : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (CodigoAnimal == "G" || CodigoAnimal =="g")
-        {
-            racionAnimalActual = racionGatos;
+        if (CantidadDias < 3) {
+            Debug.Log("cantidad de dias no valida");
         }
-        else if (CodigoAnimal == "PP" || CodigoAnimal == "pp")
+        else
         {
-            racionAnimalActual = racionPerrosChicos;
-        }
-        else if (CodigoAnimal == "PG" ||CodigoAnimal == "pg")
-        {
-            racionAnimalActual = racionPerrosGrandes;
+            if (CodigoAnimal == "G" || CodigoAnimal == "g")
+            {
+                racionAnimalActual = racionGatos;
+            }
+            else if (CodigoAnimal == "PP" || CodigoAnimal == "pp")
+            {
+                racionAnimalActual = racionPerrosChicos;
+            }
+            else if (CodigoAnimal == "PG" || CodigoAnimal == "pg")
+            {
+                racionAnimalActual = racionPerrosGrandes;
 
+            }
+            else
+            {
+                Debug.Log("tipo de animal mal ingresado, por favor intente otra vez, igrese PG para perro grande, PP para perro pequeño y G para un gato");
+                return;
+            }
+            cantidadComidaTotal = racionAnimalActual * CantidadDias;
+
+            Debug.Log("para ese periodo se necesitan: " + cantidadComidaTotal + " gramos de alimento");
+            costoAlimento = cantidadComidaTotal / 100 * precioPorcienG;
+            Debug.Log(" el precio es de: " + costoAlimento + " pesos");
         }
-        else {
-            Debug.Log("tipo de animal mal ingresado, por favor intente otra vez, igrese PG para perro grande, PP para perro pequeño y G para un gato");
-        }
-        cantidadComidaTotal = racionAnimalActual * CantidadDias;
-        
-        Debug.Log("para ese periodo se necesitan: " + cantidadComidaTotal + " gramos de alimento");
-        costoAlimento = cantidadComidaTotal / 100 * precioPorcienG;
-        Debug.Log(" el precio es de: " + costoAlimento + " pesos");
     }
 
     // Update is called once per frame
